820481d0bdae5a9681ff7344d8a94d06d19f5981
moubenben <63037909+moubenben@users.noreply.github.com>
2020-04-21 19:30:48 +0800

    new?

--- message/settings.py
+++ message/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_^mf3_qdyn+-y)jr2mpb5h@#x*4*uaco-l1eo8f+efp*xykrze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
